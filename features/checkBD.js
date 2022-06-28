91f9f6f9512af0be0b1a0844f231f1c8ca4bf37b
Artem Gostischev <phgang@ya.ru>
2022-06-29 01:57:44 +0500

    Changing the checker timer

--- features/checkBD.js
+++ features/checkBD.js
@@ -6,10 +6,10 @@ module.exports = async (client) => {
         const newMessages = await NewMessageController.findAll();
 
         newMessages.forEach(newMessage => {
-            if (isPast(addMinutes(newMessage.commandDate, 1))) {
+            if (isPast(addMinutes(newMessage.commandDate, 60))) {
                 NewMessageController.deleteNewMessage({ guildID: newMessage.guildID, userID: newMessage.userID });
             }
-        }).catch(error => console.log(error));
+        });
     }, 1000 * 60);
 };
 
